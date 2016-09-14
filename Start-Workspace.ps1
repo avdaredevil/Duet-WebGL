73cca812c252ac373fa3e6b7d27a1a68741e8185
Apoorv Verma [AP] <avdaredevil@gmail.com>
2016-09-14 19:04:31 -0400

    Got P2 Engine working! Yu'd be proud @Max00355

--- Start-Workspace.ps1
+++ Start-Workspace.ps1
@@ -1,5 +1,6 @@
 cc php,bower,Node-Extensions
 function global:server {start cmd "/c cd package& php -S localhost:8080"}
+function global:game {nw .}
 start gulp
 Write-AP "xns![Waiting]"
 sleep 10;Write-AP "ns+ Gulp Complete...";server
