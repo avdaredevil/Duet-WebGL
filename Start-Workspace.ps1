5d0847e76e295f352d78e636856d3e5209bf7bbd
Apoorv Verma [AP] <avdaredevil@gmail.com>
2016-09-19 18:58:44 -0400

    Tween Fixed, Console ops decreased

--- Start-Workspace.ps1
+++ Start-Workspace.ps1
@@ -3,4 +3,4 @@ function global:server {start cmd "/c cd package& php -S localhost:8080"}
 function global:game {nw .}
 start gulp
 Write-AP "xns![Waiting]"
-sleep 10;Write-AP "ns+ Gulp Complete...";server
+do {sleep 1} while (test-path "$PSScriptRoot/package");Write-AP "ns+ Gulp Complete...";server
