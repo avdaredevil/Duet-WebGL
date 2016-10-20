4cc422ea10ea4e780060e3261e3fcc886ca80e6b
Apoorv Verma [AP] <avdaredevil@gmail.com>
2016-10-20 18:25:50 -0400

    Added code

--- Start-Workspace.ps1
+++ Start-Workspace.ps1
@@ -1,6 +1,8 @@
+param([Switch]$OnlyImports)
 cc php,bower,Node-Extensions
 function global:server {start cmd "/c cd package& php -S localhost:8080"}
 function global:game {nw .}
+if ($OnlyImports) {exit}
 start gulp
 Write-AP "xns![Waiting]"
 do {sleep 1} while ((test-path "./package/main.html") -and (New-TimeSpan (ls .\package\main.html | % LastWriteTime) (Get-Date) | % TotalMinutes) -gt 5);Write-AP "ns+ Gulp Complete...";server
