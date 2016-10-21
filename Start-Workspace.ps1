76d5d265d94046ccac354ccccfacef20e1424b9d
Apoorv Verma [AP] <avdaredevil@gmail.com>
2016-10-21 10:51:22 -0400

    Refactored Naming

--- Start-Workspace.ps1
+++ Start-Workspace.ps1
@@ -1,8 +1,9 @@
 param([Switch]$OnlyImports)
 cc php,bower,Node-Extensions
-function global:server {start cmd "/c cd package& php -S localhost:8080"}
+function global:server {start cmd "/c cd game& php -S localhost:8080"}
 function global:game {nw .}
 if ($OnlyImports) {exit}
 start gulp
+$GameSrc = Path-Join . "game\index.html"
 Write-AP "xns![Waiting]"
-do {sleep 1} while ((test-path "./package/main.html") -and (New-TimeSpan (ls .\package\main.html | % LastWriteTime) (Get-Date) | % TotalMinutes) -gt 5);Write-AP "ns+ Gulp Complete...";server
+do {sleep 1} while ((test-path $GameSrc) -and (New-TimeSpan (ls $GameSrc | % LastWriteTime) (Get-Date) | % TotalMinutes) -gt 5);Write-AP "ns+ Gulp Complete...";server
