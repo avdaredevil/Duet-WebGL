3043aebd9cb2b6cdfa251e11907e61fd97bbebdf
Apoorv Verma [AP] <avdaredevil@gmail.com>
2016-10-25 19:17:35 -0400

    Join Path...

--- Start-Workspace.ps1
+++ Start-Workspace.ps1
@@ -4,6 +4,6 @@ function global:server {start cmd "/c cd game& php -S localhost:8080"}
 function global:game {nw .}
 if ($OnlyImports) {exit}
 start gulp
-$GameSrc = Path-Join . "game\index.html"
+$GameSrc = Join-Path . "game\index.html"
 Write-AP "xns![Waiting]"
 do {sleep 1} while ((test-path $GameSrc) -and (New-TimeSpan (ls $GameSrc | % LastWriteTime) (Get-Date) | % TotalMinutes) -gt 5);Write-AP "ns+ Gulp Complete...";server
